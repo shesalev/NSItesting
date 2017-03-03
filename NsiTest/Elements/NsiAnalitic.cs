65fe21127da3f63e2fdece4130f4dbe1fa907733
shesalev <lioliky@gmail.com>
2017-03-03 12:58:16 +0300

    Закончена версия тестирования параметра класса с грязным кодом

--- NsiTest/Elements/NsiAnalitic.cs
+++ NsiTest/Elements/NsiAnalitic.cs
@@ -3,6 +3,7 @@ using OpenQA.Selenium;
 using NsiTest.Fields;
 using System.Xml.Linq;
 using Swd.Core.WebDriver;
+using OpenQA.Selenium.Support.UI;
 
 namespace NsiTest.Elements
 {
@@ -15,27 +16,28 @@ namespace NsiTest.Elements
         public override void SetValue(NsiElementFieldValue pValue)
         {
             var lElement = pValue.Element;
-            Console.WriteLine("NsiAnalitic: "+ lElement.HasElements);
 
             foreach (XElement analiticEl in lElement.Elements("analitic"))
             {
-                var idEl = analiticEl.Element("id").Value;
                 var periodEl = analiticEl.Element("period").Value;
-                var valueEl = analiticEl.Element("value").Value;
-                Console.WriteLine("id: " + idEl);
-                Console.WriteLine("period: " + periodEl);
-                Console.WriteLine("value: " + valueEl);
-                Console.WriteLine("option: " + valueEl + "_" + idEl);
+                var optValue = analiticEl.Element("value").Value + "_" + analiticEl.Element("id").Value;
 
-                //SwdBrowser.Driver.FindElement(By.Id(pId))
-                //Console.WriteLine(Driver.FindElements(By.XPath(".//option[@value='"+valueEl + "_" + idEl+"']")).Count);
-                var lAnlList = Element.FindElements(By.XPath(".//option[@value='" + valueEl + "_" + idEl + "']"));
-                Console.WriteLine(lAnlList.Count);
+                var lAnlList = Element.FindElements(By.XPath(".//option[@value='" + optValue + "']"));
+
                 foreach (IWebElement lAnl in lAnlList)
                 {
-                    Console.WriteLine(lAnl.GetPatent().GetAttribute("id"));
+                    SelectElement lSelectList = new SelectElement(lAnl.GetPatent());
+                    var lPeriod = lAnl.GetPatent().GetPatent().GetAttribute("headers");
+
+                    if (lPeriod.Equals(periodEl))
+                    {
+                        if (lSelectList.WrappedElement.IsDisplayedSafe())
+                        {
+                            lSelectList.SelectByValue(optValue);
+                        }
+                    }
                 }
-            }            
+            }
         }
 
     }
